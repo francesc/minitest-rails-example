42831d4d0dff83cd24c3ed2f80bb7959626f7397
phiggins <pete@peterhiggins.org>
2011-08-26 01:02:58 -0700

    Initial commit.
    `rails new dummy_minitest --skip-test-unit`
    Add therubyracer to Gemfile.
    Add minitest-rails to Gemfile
    `bundle`
    `rails generate mini_test:rails
    `rake db:migrate`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyMinitest::Application.config.secret_token = 'd3767e225e0e6d814e684916d8350ead70990907c0391314c80c62f5fd2a1aa57cbf14c37cfe3b6c80d8b04b0333685abde77c55f5840490c2346624cc26a6a2'
