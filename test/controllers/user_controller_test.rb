bbff312c1a6272cae9fcf5902141fe4abca0fe66
phiggins <pete@peterhiggins.org>
2011-08-28 23:18:46 -0700

    `rails generate scaffold user`

---
+++ test/controllers/user_controller_test.rb
@@ -0,0 +1,49 @@
+require "minitest_helper"
+
+class UserControllerTest < MiniTest::Rails::Controller
+  setup do
+    @user = users(:one)
+  end
+
+  def test_index
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
+  def test_new
+    get :new
+    assert_response :success
+  end
+
+  def test_create
+    assert_difference('User.count') do
+      post :create, user: @user.attributes
+    end
+
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  def test_show
+    get :show, id: @user.to_param
+    assert_response :success
+  end
+
+  def test_edit
+    get :edit, id: @user.to_param
+    assert_response :success
+  end
+
+  def test_update
+    put :update, id: @user.to_param, user: @user.attributes
+    assert_redirected_to user_path(assigns(:user))
+  end
+
+  def test_destroy
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @user.to_param
+    end
+
+    assert_redirected_to users_path
+  end
+end
