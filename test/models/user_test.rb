e79316e1bc29a33009830c64a273b1bbe0f56505
Wojciech Mach <wojtek@wojtekmach.pl>
2011-11-22 01:22:30 +0100

    Upgrade to minitest-matchers 1.1.2

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,8 @@ require "test_helper"
 class UserTest < MiniTest::Rails::Model
   subject { User.new }
 
-  it { must have_valid(:first_name).when("Good") }
-  it { wont have_valid(:first_name).when(nil, "") }
-
-  it { must have_valid(:last_name).when("Good") }
-  it { wont have_valid(:last_name).when(nil, "") }
+  must { have_valid(:first_name).when("Good") }
+  wont { have_valid(:first_name).when(nil, "") }
 
   it "#name returns first and last name" do
     user = User.new :first_name => "Joe", :last_name => "Doe"
